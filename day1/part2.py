ab941aecd30097ab67d68fcba4880be6acaef440
Jonas Kittner <jonas.kittner@rub.de>
2022-12-03 03:06:30 +0100

    add day1 part 2

---
+++ day1/part2.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import argparse
+from collections.abc import Sequence
+
+
+def _solve(data: str) -> int:
+    result = []
+    current = 0
+    for cal in data.splitlines():
+        if cal != '':
+            current += int(cal)
+        else:
+            result.append(current)
+            current = 0
+
+    result.append(current)
+    return sum(sorted(result, reverse=True)[:3])
+
+
+TEST_INPUT = '''\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+
+
+def test_solve():
+    assert _solve(TEST_INPUT) == 45000
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    args = parser.parse_args(argv)
+    with open(args.input) as f:
+        data = f.read()
+
+    print(f'the solution is: {_solve(data)}')
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
